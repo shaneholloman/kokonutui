3d890123234bb8e3c56c324fef75041abe57001c
Dorian Baffier <hello@dorianbaffier.com>
2025-12-11 18:53:40 +0900

    feat: add live github stars

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,27 +1,30 @@
+import type { Metadata } from "next";
+import { Suspense } from "react";
 import { Header } from "@/components/landing/header";
 import { Footer } from "@/components/layout/footer";
-import type { Metadata } from "next";
 // import { usePathname } from "next/navigation";
 
 export const metadata: Metadata = {
-    title: {
-        template: "%s | Kokonut UI - Open Source Components",
-        default: "Kokonut UI - Open Source Components",
-    },
+  title: {
+    template: "%s | Kokonut UI - Open Source Components",
+    default: "Kokonut UI - Open Source Components",
+  },
 };
 
 export default function HomeLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <>
-            <Header />
-            <main className="relative w-full pt-0 md:pt-0 bg-white dark:bg-black">
-                {children}
-            </main>
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Header />
+      </Suspense>
+      <main className="relative w-full bg-white pt-0 md:pt-0 dark:bg-black">
+        {children}
+      </main>
+      <Footer />
+    </>
+  );
 }
